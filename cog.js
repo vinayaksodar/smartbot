445275b9c1a4d595a3c3a9fb2c236b97fc2e9367
AshwinTC <cs16b033@smail.iitm.ac.in>
2017-10-12 22:17:47 +0530

    Corrected code

--- cog.js
+++
@@ -1,6 +0,0 @@
-const temp = require('./temp1.js');
-
-const url = "http://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/m(01-32)_gr.jpg";
-
-body =  temp.getCaptionFromUrl(url);
-console.log(body);
